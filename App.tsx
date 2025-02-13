c3633560dda7d0452b40e74d1ca581e38d035a60
Timonwa Akintokun <timonwaakintokun@gmail.com>
2025-02-13 15:03:14 +0100

    feat: add new screens and navigation for dashboard features

--- App.tsx
+++ App.tsx
@@ -8,9 +8,21 @@ import ResetPasswordScreen from "./src/screens/(auth)/reset-password";
 import ForgotPasswordScreen from "./src/screens/(auth)/forgot-password";
 import OTPVerificationScreen from "./src/screens/(auth)/otp-verification";
 import ProfileSetupScreen from "./src/screens/(dashboard)/profile-setup";
+import NotificationsScreen from "./src/screens/(dashboard)/notifications";
+import HomeScreen from "./src/screens/(dashboard)/home";
+import ProfileDetailsScreen from "./src/screens/(dashboard)/profile";
+import AllMessagesScreen from "./src/screens/(dashboard)/messages";
+import AllAppointmentsScreen from "./src/screens/(dashboard)/appointments";
+import AllPatientsScreen from "./src/screens/(dashboard)/patients";
+import SearchScreen from "./src/screens/(dashboard)/search";
+import ViewMessageScreen from "./src/screens/(dashboard)/messages/[id]";
+import ViewAppointmentScreen from "./src/screens/(dashboard)/appointments/[id]";
+import ViewPatientScreen from "./src/screens/(dashboard)/patients/[id]";
 import BottomTabs from "./src/Navigation/BottomTabs";
 import { Provider } from "react-redux";
 import { store } from "./src/integrations/store";
+import { Alert, TouchableOpacity } from "react-native";
+import { Text } from "react-native-gesture-handler";
 
 const Stack = createStackNavigator();
 
@@ -84,6 +96,25 @@ export default function App() {
               headerTitle: "",
             }}
           />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen
+            name="ProfileDetails"
+            component={ProfileDetailsScreen}
+          />
+          <Stack.Screen name="AllMessages" component={AllMessagesScreen} />
+          <Stack.Screen
+            name="AllApointments"
+            component={AllAppointmentsScreen}
+          />
+          <Stack.Screen name="AllPatients" component={AllPatientsScreen} />
+          <Stack.Screen name="Notifications" component={NotificationsScreen} />
+          <Stack.Screen name="Search" component={SearchScreen} />
+          <Stack.Screen name="ViewMessage" component={ViewMessageScreen} />
+          <Stack.Screen
+            name="ViewAppointment"
+            component={ViewAppointmentScreen}
+          />
+          <Stack.Screen name="ViewPatient" component={ViewPatientScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
--- src/screens/(dashboard)/notifications.tsx
+++ src/screens/(dashboard)/notifications.tsx
@@ -2,14 +2,11 @@ import { View } from "react-native";
 import React from "react";
 import globalStyles from "@/src/styles/global";
 import { notificationsData } from "@/src/helpers";
-import { ScreenTitle } from "@/src/components/common";
 import NotificationList from "@/src/components/notifications/NotificationList";
 
 const NotificationsScreen = () => {
   return (
     <View style={globalStyles.container}>
-      <ScreenTitle title="Notifications" headerLeft />
-
       <NotificationList notificationsData={notificationsData} />
     </View>
   );

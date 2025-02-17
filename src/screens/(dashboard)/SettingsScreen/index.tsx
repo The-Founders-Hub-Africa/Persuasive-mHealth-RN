0bd6efdef389d940a302f330a35306993d44461b
Timonwa Akintokun <timonwaakintokun@gmail.com>
2025-02-17 20:43:14 +0100

    add logout button

--- src/screens/(dashboard)/SettingsScreen/index.tsx
+++ src/screens/(dashboard)/SettingsScreen/index.tsx
@@ -1,15 +1,39 @@
-import { ScrollView, View, Text } from "react-native";
+import {
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import React from "react";
 import globalStyles from "@/src/styles/global";
+import theme from "@/src/styles/theme";
 
 const SettingsScreen = () => {
   return (
     <ScrollView>
       <View style={globalStyles.dashboardContainer}>
-        <Text>Settings Screen</Text>
+        <TouchableOpacity style={styles.submitButton}>
+          <Text style={styles.submitText}>Logout</Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
 };
 
 export default SettingsScreen;
+
+const styles = StyleSheet.create({
+  submitButton: {
+    backgroundColor: "#B91C1C",
+    padding: 12,
+    borderRadius: 10,
+    alignItems: "center",
+    width: "100%",
+  },
+  submitText: {
+    color: theme.colors.white,
+    fontSize: 16,
+    fontWeight: 600,
+  },
+});

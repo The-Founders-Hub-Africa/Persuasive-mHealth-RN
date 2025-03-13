ee87022c752639722238fcb95d3258bb47a89c85
timonwa <timonwaakintokun@gmail.com>
2025-03-13 16:19:10 +0100

    Update PatientProps interface to include kin_number and correct identifier formatting for improved type consistency.

--- src/types/patients.ts
+++ src/types/patients.ts
@@ -4,7 +4,7 @@ export interface PatientProps {
   id: number;
   full_name: string;
   whatsapp_number: string;
-  identifier:string,
+  identifier: string;
   image: ImageSourcePropType | undefined;
   document: ImageSourcePropType | undefined;
   date: string;
@@ -12,6 +12,7 @@ export interface PatientProps {
   address: string;
   medical_practitioner: number;
   next_of_kin: string;
+  kin_number: string;
   about: string;
   genotype: string;
   gender: string;
